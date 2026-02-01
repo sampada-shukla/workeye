95c15269d0d2a2b92b3c9f22698fc72d59b9746c
sampada-shukla <sampada.s@rajlaxmiworld.com>
2026-02-01 21:28:22 +0530

    Update Tutorial_Page.tsx

--- src/components/Tutorial_Page.tsx
+++ src/components/Tutorial_Page.tsx
@@ -535,9 +535,11 @@ export default function TutorialPage() {
     boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)'
   }}>
     <TutorialVideo />
-  </div>
-</div>
-</motion.div>
+                </div>
+              </motion.div>
+            </div> 
+          </div> 
+        </section>
 
         {/* Tutorial Section Header */}
         <section
