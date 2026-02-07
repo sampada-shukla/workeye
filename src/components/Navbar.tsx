6d1891c8bd794e1942dc6e70359cc5214a671f10
sampada-shukla <sampada.s@rajlaxmiworld.com>
2026-02-07 16:15:19 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Button } from "./ui/button";
-import logoImage from "../assets/workeye.jpg";
+import logoImage from "../assets/workeye.png";
 
 interface NavbarProps {
   onLoginClick: () => void;
