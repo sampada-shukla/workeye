bb2d3411f806bed32f6a2034af5d0ab226319bfb
sampada-shukla <sampada.s@rajlaxmiworld.com>
2026-02-11 15:30:18 +0530

    Update Tutorial_Page.tsx

--- src/components/Tutorial_Page.tsx
+++ src/components/Tutorial_Page.tsx
@@ -1120,6 +1120,7 @@ export default function TutorialPage() {
                 style={{
                   maxWidth: isMobile ? '100%' : '650px',
                   marginTop: isMobile ? '0' : '-2.5rem',
+                  marginLeft: isMobile ? '0' : '-3rem',
                 }}
               >
                 <motion.h1
